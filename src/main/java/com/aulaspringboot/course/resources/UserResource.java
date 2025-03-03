9f5d829bfa19db80b51f7478aaa1766d11e6108b
leandroomena18 <164086583+leandroomena18@users.noreply.github.com>
2025-03-03 12:06:23 -0300

    H2 database, test profile, JPA

--- src/main/java/com/aulaspringboot/course/resources/UserResource.java
+++ src/main/java/com/aulaspringboot/course/resources/UserResource.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aulaspringboot.course.entities.User;
 
-@RestController //recurso web impl por controlador rest
-@RequestMapping(value = "/users") //nomeando o recurso
+@RestController //recurso web implementado por controlador rest
+@RequestMapping(value = "/users") //nomeando o recurso - usa esse nome no localhost8080/users
 public class UserResource {
 	
 	//class para testar se o recurso está funcionado 
 	
-	@GetMapping //responde a uma requisição do tipo get do http
+	@GetMapping //indica que responde a uma requisição do tipo get do http
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maria Clara", "MariaClara@gmail.com", "999999", "12345");
 		return ResponseEntity.ok().body(u);
+		
+		//responseEntity classe que retorna requisições web
 		//.ok() retorna resposta com sucesso no http
 		//.body(<user) retorna o corpo do usuário que acabou de instanciar
 	}
